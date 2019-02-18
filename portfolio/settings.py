aaada77a1d7b022aa50bb640eaf9fa6fbc4ad98e
sangenispablo@gmail.com <sangenispablo@gmail.com>
2019-02-18 15:57:27 -0300

    Agrego Jobs App y Modelo

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,10 +74,21 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': '9216',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -118,3 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Esta constante indica donde se van a guardar los archivos de imagenes y demas
+# Cuando en algun modelo exista un campo de tipo ImageField(upload_to='media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# La constante que sigue indica como nos vamos a referir a esa imagen que se guardo en la BD
+# no necesariamente tiene que ser la misma que el directorio donde se guarda la imagen
+MEDIA_URL = '/media/'
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+# Lo que se agrego despues de cerrar el corchete de urlpatterns es para
+# poder servir los archivos que se agregan en media/
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
